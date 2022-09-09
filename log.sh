4568c3dc1e9763dee8d1dec4a2c35ecd89fac488
gideonthomasd <56291811+gideonthomasd@users.noreply.github.com>
2022-09-09 12:31:38 +0100

    Add files via upload

--- log.sh
+++ log.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
 # options to be displayed
-option0="logout"
-option1="lock"
-option2="suspend"
-option3="scheduled suspend (10min)"
-option4="scheduled suspend (20min)"
-option5="scheduled suspend (30min)"
-option6="reboot"
-option7="shutdown"
+option0=" logout"
+option1="望 lock"
+option2=" suspend"
+option3=" scheduled suspend (10min)"
+option4=" scheduled suspend (20min)"
+option5=" scheduled suspend (30min)"
+option6=" reboot"
+option7="⏻ shutdown"
 
 # options passed into variable
 options="$option0\n$option1\n$option2\n$option3\n$option4\n$option5\n$option6\n$option7"
 
-chosen="$(echo -e "$options" | rofi -lines 8 -dmenu -p "power")"
+chosen="$(echo -e "$options" | rofi -lines 8 -dmenu -p "power" -font "mononoki Nerd Font 23")"
 case $chosen in
     $option0)
 		#i3-msg exit;;
-		pkill dwm || pkill i3 || pkill bspwm || pkill openbox || pkill herbstluftwm || pkill awesome ;;
+		pkill dwm || pkill i3 || pkill bspwm || pkill openbox || pkill herbstluftwm || pkill awesome || pkill qtile ;;
     $option1)
         i3lock-fancy;;
     $option2)
